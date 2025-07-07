bd34b5e79a7a93d12c27b07e78e91347aa05b02c
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-07-07 17:26:41 -0300

    fix: fixed service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/messages";
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080/messages";
 
 export async function enviarFormulario(
   data: any
