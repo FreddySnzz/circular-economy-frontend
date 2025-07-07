ac604cbec1108615014f6fe7da8bcffa94e5af41
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-07-07 17:31:33 -0300

    fix: fixed footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
       <Navbar className="fixed top-0 left-0 w-full z-50"/>
       <CircularInfoSection />
       <SimpleForm />
-      <Footer />
+      <div className="max-sm:text-xs">
+        <Footer />
+      </div>
     </main>
   );
 }
